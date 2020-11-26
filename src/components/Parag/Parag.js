70e32aa6be783e8b76dd89faec7878fa68df76a5
aymanouch <aymanouchedih1997@gmail.com>
2020-11-25 14:34:48 -1000

    media query to the complexe title and logos

--- src/components/Parag/Parag.js
+++ src/components/Parag/Parag.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import './Parag.css';
 const Parag = (props) => {
+    console.log(props.simpleTitle);
     return(
-        <div className={`parag signika ${props.simpleTitle > 0 ? 'with-title': ''} ${props.bigbig ? 'big-para__height' : ''}`}>
-               {((!props.simpleTitle)&&!props.bigbig)&&<h2>{props.smallTitle}</h2>}
+        <div className={`parag signika ${(!props.simpleTitle && !props.bigbig) ? 'with-title': ''} ${props.bigbig ? 'big-para__height' : ''}`}>
+               {((!props.simpleTitle)&&!props.bigbig)&&<h2 className="marvel">{props.smallTitle}</h2>}
                <p>{props.parag}</p>
         </div>
     );
