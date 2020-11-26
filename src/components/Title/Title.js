a95139583621175f1bc49b07a905096103d77bb4
aymanouch <aymanouchedih1997@gmail.com>
2020-11-25 22:49:01 -1000

    fix bugs in big title

--- src/components/Title/Title.js
+++ src/components/Title/Title.js
@@ -4,7 +4,7 @@ const Title = (props) => {
     return(
     <div className={`title ninuto ${props.bigbig ? 'bigbig' : props.simpleTitle ? 'simple-title' : 'complexe-title'}`}>
              <h1 >
-                 {(props.simpleTitle)&&<span className="small-title simple-title marvel">{props.smallTitle}</span>}<br />
+                 {(props.simpleTitle)&&<><span className="small-title simple-title marvel">{props.smallTitle}</span><br /></>}
                  {props.bigTitle}<br />
                  {props.bigbig&&<span className='ohter-title'>{props.otherTitle}</span>}
              </h1>
